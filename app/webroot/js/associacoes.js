37b3d53b99ac29ac517dd37213324fdb859af99a
GabrielR1B <gabrielmartinsmiranda800@gmail.com>
2025-10-06 11:04:09 -0300

    add associacoes.js

---
+++ app/webroot/js/associacoes.js
@@ -0,0 +1,56 @@
+/**
+ * Inicializa um campo de autocomplete para associar dados de um modelo relacionado.
+ *
+ * @param {string} modelName - O nome do Modelo em PascalCase (ex: 'Inventor', 'Depositante').
+ * @param {string} searchInputId - O ID do campo de texto para a busca (ex: 'buscar_inventor').
+ * @param {string} displayListId - O ID do <ul> ou <div> onde os itens selecionados serão exibidos.
+ * @param {string} hiddenContainerId - O ID do <div> que armazenará os inputs ocultos com os IDs.
+ * @param {string} autocompleteUrl - A URL completa para a action AJAX que retorna a lista de sugestões.
+ */
+function associacaoAutocomplete(modelName, searchInputId, displayListId, hiddenContainerId, autocompleteUrl) {
+
+    // 1. Configura o Autocomplete no campo de busca
+    $('#' + searchInputId).autocomplete({
+        source: autocompleteUrl,
+        minLength: 2,
+        select: function(event, ui) {
+            var itemId = ui.item.id;
+            var itemNome = ui.item.nome || ui.item.value;
+
+            if ($('#' + modelName.toLowerCase() + '_' + itemId).length === 0) {
+                adicionarItemNaLista(itemId, itemNome);
+            }
+
+            $(this).val('');
+            return false;
+        }
+    });
+
+    // 2. Função interna para adicionar o item na lista visual e nos inputs ocultos
+    function adicionarItemNaLista(id, nome) {
+        var itemVisual = "<li id='item_visual_" + modelName.toLowerCase() + "_" + id + "'>" + 
+                         nome + 
+                         " <a href='#' class='remover-item-associado' data-model='" + modelName + "' data-id='" + id + "'> (remover)</a>" +
+                         "</li>";
+        $('#' + displayListId).append(itemVisual);
+
+        var inputOculto = "<input type='hidden' " +
+                          "name='data[" + modelName + "][" + modelName + "][]' " +
+                          "value='" + id + "' " +
+                          "id='" + modelName.toLowerCase() + '_' + id + "'>";
+        $('#' + hiddenContainerId).append(inputOculto);
+    }
+
+    // 3. Lógica para o botão 'remover'
+    $(document).on('click', '.remover-item-associado', function(e) {
+        e.preventDefault();
+        
+        var clickedModel = $(this).data('model');
+        
+        if (clickedModel === modelName) {
+            var itemId = $(this).data('id');
+            $('#' + modelName.toLowerCase() + '_' + itemId).remove();
+            $('#item_visual_' + modelName.toLowerCase() + '_' + itemId).remove();
+        }
+    });
+}
